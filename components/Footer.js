36c414b9c886419c0ed7eefd07006804b7378d7c
yash5201 <51786748+yash5201@users.noreply.github.com>
2022-06-21 01:32:31 +0530

    color , logo

--- components/Footer.js
+++ components/Footer.js
@@ -1,32 +1,32 @@
 /*import Link from 'next/link'
 function Footer() {
-    return (
-      <>
-        <div className='top-100 bg-orange-900 flex justify-between w-full items-center  bottom-0 left-0'>
+	return (
+	  <>
+		<div className='top-100 bg-orange-900 flex justify-between w-full items-center  bottom-0 left-0'>
     
-            <div></div>
-            <div>
-            
-                <h1 className='text-white font-bold'>ABOUT US</h1>
-                <h3 className='text-white'> 100k+ orders delivered globally. World class products, Proudly made in India.  </h3>
-                <h3 className='text-white'>Customer Support – support@xyz.com</h3>
+			<div></div>
+			<div>
+		    
+				<h1 className='text-white font-bold'>ABOUT US</h1>
+				<h3 className='text-white'> 100k+ orders delivered globally. World class products, Proudly made in India.  </h3>
+				<h3 className='text-white'>Customer Support – support@xyz.com</h3>
 
-            </div>
-            <div>
-              <h1 className='text-white font-bold justify-between w-full items-center'>POLICES</h1>      
-              <Link href='/Return'><h3 className='text-white '>Return & Cancellation Policy</h3></Link>
-              <Link href='/Terms'><h3 className='text-white'>Terms & Conditions</h3></Link>
-              <Link href='/Privacy_Policy'><h3 className='text-white'>Privacy Policy</h3></Link>
-              <Link href='/Shipping_Policy'><h3 className='text-white'>Shipping Policy</h3></Link>
-              <Link href='/Contact_Us'><h3 className='text-white'>Contact Us</h3></Link>
-            </div>
-            <div></div>
-            
-          
+			</div>
+			<div>
+			  <h1 className='text-white font-bold justify-between w-full items-center'>POLICES</h1>      
+			  <Link href='/Return'><h3 className='text-white '>Return & Cancellation Policy</h3></Link>
+			  <Link href='/Terms'><h3 className='text-white'>Terms & Conditions</h3></Link>
+			  <Link href='/Privacy_Policy'><h3 className='text-white'>Privacy Policy</h3></Link>
+			  <Link href='/Shipping_Policy'><h3 className='text-white'>Shipping Policy</h3></Link>
+			  <Link href='/Contact_Us'><h3 className='text-white'>Contact Us</h3></Link>
+			</div>
+			<div></div>
+		    
+		  
     
-        </div>
-      </>
-    )
+		</div>
+	  </>
+	)
   }
   
   export default Footer
@@ -40,17 +40,17 @@ import { FaWhatsapp } from "react-icons/fa";
 function Footer() {
 	return (
 		<>
-			<div className=" bg-[#99582A] h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
+			<div className=" bg-[#F4E9CD] h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
 				<div className="p-5 ">
 					<ul>
 						<p className="text-black-1000 font-bold text-3xl pb-6">
-						<span className="text-white">Dekhanee: </span>
-						<span className="text-[#FFE6A7]">The Beautiful</span>
+							<span className="text-[#65968d]">Dekhanee: </span>
+							<span className="text-[#172f3d]">The Beautiful</span>
 						</p>
 						<div className="flex gap-6 pb-5">
-						<FaInstagram className="text-2xl cursor-pointer hover:text-blue-600" />
-						<Link href='https://www.facebook.com/Dekhanee'><FaFacebook className="text-2xl cursor-pointer hover:text-blue-600" /></Link>
-              				<FaWhatsapp className="text-2xl cursor-pointer hover:text-blue-600" />
+							<FaInstagram className="text-2xl cursor-pointer hover:text-blue-600" />
+							<Link href='https://www.facebook.com/Dekhanee'><FaFacebook className="text-2xl cursor-pointer hover:text-blue-600" /></Link>
+							<FaWhatsapp className="text-2xl cursor-pointer hover:text-blue-600" />
 						</div>
 					</ul>
 				</div>
@@ -77,16 +77,16 @@ function Footer() {
 					<ul>
 						<p className="text-black font-bold text-2xl pb-4">Support</p>
 						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-            			<Link href='/contact'> Contact Us </Link>
+							<Link href='/contact'> Contact Us </Link>
 						</li>
 					</ul>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center text-center  p-5 bg-[#BB9457]">
+			<div className="flex flex-col justify-center items-center text-center  p-5 bg-[#468189]">
 				<h1 className=" text-gray-800 font-semibold">
 					© 2021-2022 All rights reserved | Built with ❤ by{" "}
 					<span className="hover:text-blue-600 font-semibold cursor-pointer">
-        			Project Aspects {" "}
+						Project Aspects {" "}
 					</span>
 				</h1>
 			</div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import Image from 'next/image'
+import logo from '../public/Dekhanee-Logo.png'
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
@@ -8,46 +10,51 @@ const Navbar = () => {
 
     return (
         <>
-        <div className="flex-1">
+            <div className="flex-1">
 
-            <nav className=" top-0 flex items-center h-32 w-full text-sm bg-[#99582A] px-2 md:px-5 lg:px-10">
+                <nav className=" top-0 flex items-center h-32 w-full text-sm bg-[#F4E9CD] px-2 md:px-5 lg:px-10">
 
-                <div className="dropdown">
-                    <button className="bg-transparent text-white font-semibold py-2 px-4 rounded inline-flex items-center">
-                        <svg className="w-8 h-8" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    <ul className="link-container dropdown-menu hidden mr-2 md:mr-5 lg:mr-10">
-                        <Link href="/home"><a className="rounded-t navbar-link border border-gray-700">Home</a></Link>
-                        <Link href="/about"><a className="navbar-link border border-gray-700 border-t-0">About</a></Link>
-                        <Link href="/products"><a className="navbar-link border border-gray-700 border-t-0">Products</a></Link>
-                    </ul>
-                    </button>
-                  </div>
-                  
-                <div className="flex-1 p-5">
-                    <Link href="/"><h1 className="text-white text-center font-bold"></h1></Link>
-                </div>
+                    <div className="dropdown">
+                        <button className="bg-transparent text-[#031926] font-semibold py-2 px-4 rounded inline-flex items-center">
+                            <svg className="w-8 h-8" fill="none" stroke="#031926" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                            <ul className="link-container dropdown-menu hidden mr-2 md:mr-5 lg:mr-10">
+                                <Link href="/home"><a className="rounded-t navbar-link border border-gray-700">Home</a></Link>
+                                <Link href="/about"><a className="navbar-link border border-gray-700 border-t-0">About</a></Link>
+                                <Link href="/products"><a className="navbar-link border border-gray-700 border-t-0">Products</a></Link>
+                            </ul>
+                        </button>
+                    </div>
+
+                    <div className="flex-1 p-5">
+                        <Link href="/"><div className="text-center cursor-pointer"><Image
+                            src={logo}
+                            alt="Picture of the author"
+                            width="130px"
+                            height="130px"
+                        /></div></Link>
+                    </div>
 
 
-                <div className="flex ">
+                    <div className="flex ">
 
-                    <Link href="/account">
-                    <AiOutlineUser className=" text-2xl text-white cursor-pointer hover:text-yellow-600" />
-                    </Link>
-                    
-                    <Link href="/">
-                    <FiSearch className=" text-2xl text-white cursor-pointer hover:text-yellow-600" />
-                    </Link>
-                
-                
-                    <Link href="/cart">
-                    <AiOutlineShoppingCart className="text-2xl text-white cursor-pointer hover:text-yellow-600" />
-                    </Link>
-                </div>
-            
-            </nav>
-        </div>
+                        <Link href="/account">
+                            <AiOutlineUser className=" text-2xl text-[#031926] cursor-pointer hover:text-yellow-600"  />
+                        </Link>
+
+                        <Link href="/">
+                            <FiSearch className=" text-2xl text-[#031926] cursor-pointer hover:text-yellow-600" />
+                        </Link>
+
+
+                        <Link href="/cart">
+                            <AiOutlineShoppingCart className="text-2xl text-[#031926] cursor-pointer hover:text-yellow-600" />
+                        </Link>
+                    </div>
+
+                </nav>
+            </div>
         </>
     );
 }
