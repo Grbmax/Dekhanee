b55277b250967fa07acab02ba4f3592a9b57b188
rohitpanchalll <panchalrohit0021@gmail.com>
2022-06-21 22:29:35 +0530

    added cards in product categories

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Card = () => {
+    return (
+        <>
+          
+        </>
+    )
+}
+
+export default Card
+
+
+
+
+
+{/*<div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <div className="px-6 py-4">
+                <p class="text-gray-700">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+            </div>
+</div>*/}
