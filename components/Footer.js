cf865a229d5cfc3c569eb77968556ceabaea0a21
Yash Shirke <51786748+yash5201@users.noreply.github.com>
2022-08-07 02:11:22 +0530

    7-8
    about us , contact us, product , color,

--- components/Footer.js
+++ components/Footer.js
@@ -1,36 +1,3 @@
-/*import Link from 'next/link'
-function Footer() {
-	return (
-	  <>
-		<div className='top-100 bg-orange-900 flex justify-between w-full items-center  bottom-0 left-0'>
-    
-			<div></div>
-			<div>
-		    
-				<h1 className='text-white font-bold'>ABOUT US</h1>
-				<h3 className='text-white'> 100k+ orders delivered globally. World class products, Proudly made in India.  </h3>
-				<h3 className='text-white'>Customer Support – support@xyz.com</h3>
-
-			</div>
-			<div>
-			  <h1 className='text-white font-bold justify-between w-full items-center'>POLICES</h1>      
-			  <Link href='/Return'><h3 className='text-white '>Return & Cancellation Policy</h3></Link>
-			  <Link href='/Terms'><h3 className='text-white'>Terms & Conditions</h3></Link>
-			  <Link href='/Privacy_Policy'><h3 className='text-white'>Privacy Policy</h3></Link>
-			  <Link href='/Shipping_Policy'><h3 className='text-white'>Shipping Policy</h3></Link>
-			  <Link href='/Contact_Us'><h3 className='text-white'>Contact Us</h3></Link>
-			</div>
-			<div></div>
-		    
-		  
-    
-		</div>
-	  </>
-	)
-  }
-  
-  export default Footer
-*/
 import Link from "next/link";
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
@@ -40,57 +7,62 @@ import { FaWhatsapp } from "react-icons/fa";
 function Footer() {
 	return (
 		<>
-			<div className=" bg-[#F4E9CD] h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
+			<div className=" bg-[#A1C6EA] h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-15">
 				<div className="p-5 ">
+					
 					<ul>
-						<p className="text-black-1000 font-bold text-3xl pb-6">
+					<Link href={"/"}>
+						<p className="text-black-1000 font-bold text-3xl pb-6 cursor-pointer">
 							<span className="text-[#65968d]">Dekhanee: </span>
 							<span className="text-[#172f3d]">The Beautiful</span>
 						</p>
-						<div className="flex gap-6 pb-5">
-							<FaInstagram className="text-2xl cursor-pointer hover:text-blue-600" />
-							<Link href='https://www.facebook.com/Dekhanee'><FaFacebook className="text-2xl cursor-pointer hover:text-blue-600" /></Link>
-							<FaWhatsapp className="text-2xl cursor-pointer hover:text-blue-600" />
+						</Link>
+						<div className="flex gap-6 pb-4">
+							<FaInstagram className="text-2xl cursor-pointer hover:text-pink-600" />
+							<Link href='https://www.facebook.com/Dekhanee'><FaFacebook className="text-2xl cursor-pointer hover:text-pink-600" />
+							</Link>
+							<FaWhatsapp className="text-2xl cursor-pointer hover:text-pink-600" />
 						</div>
 					</ul>
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-black font-bold text-2xl pb-4">Products</p>
-						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<p className="text-black font-bold text-2xl pb-2">Products</p>
+						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-pink-600 cursor-pointer">
 							Ear Rings
 						</li>
 					</ul>
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-black font-bold text-2xl pb-4">Company</p>
-						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<p className="text-black font-bold text-2xl pb-2">Company</p>
+						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-pink-600 cursor-pointer">
 							About
 						</li>
-						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-pink-600 cursor-pointer">
 							Press & Media
 						</li>
 					</ul>
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-black font-bold text-2xl pb-4">Support</p>
-						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							<Link href='/contact'> Contact Us </Link>
+						<p className="text-black font-bold text-2xl pb-2">Support</p>
+						
+						<li className="text-gray-900 text-md pb-2 font-semibold hover:text-pink-600 cursor-pointer">
+						<Link href={"/contact"}>Contact Us </Link>
 						</li>
 					</ul>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center text-center  p-5 bg-[#131414]">
-				<h1 className=" text-white font-semibold">
+			<div className="flex flex-col justify-center items-center text-center p-3 bg-[#131414]">
+				<h6 className=" text-white font-inter">
 					© 2021-2022 All rights reserved | Built with 
-					<span className="text-red-600 font-semibold cursor-pointer"> ❤ </span>
+					<span className="text-red-600 font-inter cursor-pointer"> ❤ </span>
 					by{" "}
-					<span className="hover:text-blue-600 font-semibold cursor-pointer">
+					<span className="hover:text-blue-600 font-inter cursor-pointer">
 						Project Aspects {" "}
 					</span>
-				</h1>
+				</h6>
 			</div>
 		</>
 	);
