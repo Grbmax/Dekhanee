3b9f32b02a2d1cd3e74f770a1a4c6dfea5454f0c
Yash Shirke <51786748+yash5201@users.noreply.github.com>
2022-08-07 23:51:05 +0530

    7-8
    update

---
+++ components/Slideshow.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Zoom } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+const Slideshow = () => {
+	//Array of Images
+	const images = [
+		"/img/1.jpg",
+		"/img/2.jpg",
+		"/img/3.jpg",
+	];
+
+	//These are custom properties for zoom effect while slide-show
+	const zoomInProperties = {
+		indicators: true,
+		scale: 1,
+		duration: 3000,
+		transitionDuration: 500,
+		infinite: true,
+		prevArrow: (
+			<div style={{ width: "30px", marginRight: "-30px", cursor: "pointer" }}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 512 512"
+					fill="#2e2e2e"
+				>
+					<path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z" />
+				</svg>
+			</div>
+		),
+		nextArrow: (
+			<div style={{ width: "30px", marginLeft: "-30px", cursor: "pointer" }}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 512 512"
+					fill="#2e2e2e"
+				>
+					<path d="M512 256L270 42.6v138.2H0v150.6h270v138z" />
+				</svg>
+			</div>
+		),
+	};
+	return (
+		<div className="m-1">
+			<Zoom {...zoomInProperties}>
+				{images.map((each, index) => (
+					<div key={index} className="flex justify-center w-full min-h-screen ">
+						<img
+							className="min-h-screen object-cover rounded-lg shadow-xl"
+							src={each}
+						/>
+					</div>
+				))}
+			</Zoom>
+		</div>
+	);
+};
+
+export default Slideshow;
--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,49 @@
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Image from 'next/image'
 
 function About() {
-    return(
-        <>
-        <Navbar/>
-        <h1 className='text-3xl font-bold' >About</h1>
-        <Footer/>
-        </>
-    )
-  }
-  
-  export default About
+  return (
+    <>
+      <Navbar />
+      <div className="bg-[#131414]">
+        <section className="text-gray-400 bg-gray-900 body-font ">
+          <div className="container px-5 py-24 mx-auto flex flex-col ">
+            <div className="lg:w-4/6 mx-auto ">
+              <div className="rounded-lg h-64 overflow-hidden">
+                <Image
+                  alt="content"
+                  src="/ear-rings/2.jpeg"
+                  height={800}
+                  width= {1250}
+                  />
+              </div>
+              <div className="flex flex-col sm:flex-row mt-10">
+                <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                  <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-800 text-gray-600">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <div className="flex flex-col items-center text-center justify-center">
+                    <h2 className="font-medium title-font mt-4 text-white text-lg">Phoebe Caulfield</h2>
+                    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                    <p className="text-base text-gray-400">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+                  </div>
+                </div>
+                <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                  <p className="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90s scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default About
