ec56e7d65e732d0fd3d01504d2bde2efbba1c7d6
Yash Shirke <51786748+yash5201@users.noreply.github.com>
2022-08-17 17:09:31 +0530

    17-8

--- components/Navbar.js
+++ components/Navbar.js
@@ -60,6 +60,8 @@ const Navbar = () => {
 
 export default Navbar;
 */
+/*
+
 
 
 import React, { useState } from "react";
@@ -270,6 +272,120 @@ function Navbar() {
 				</Transition>
 			</div>
 		</div>
+	);
+}
+
+export default Navbar;*/
+
+import Link from "next/link";
+import { AiFillCloseCircle, AiOutlineMinusCircle, AiOutlinePlusCircle, AiOutlineShoppingCart } from "react-icons/ai";
+import { useRef } from "react";
+
+const Navbar = () => {
+
+	const toggleCart = () => {
+		if (ref.current.classList.contains('translate-x-full')) {
+			ref.current.classList.remove('translate-x-full')
+			ref.current.classList.add('translate-x-0')
+		}
+		else if (!ref.current.classList.contains('translate-x-full')) {
+			ref.current.classList.remove('translate-x-0')
+			ref.current.classList.add('translate-x-full')
+		}
+	}
+	const ref = useRef()
+
+	return (
+		<>
+			<nav className="absolute top-0 space-between flex items-center h-20 w-full z-50 text-sm bg-[#F4E9CD] px-2 md:px-5 lg:px-10">
+
+				<div className="flex-1 p-5">
+					<Link href="/"><img className='w-16 cursor-pointer' src="/Dekhanee-Logo.png" /></Link>
+
+				</div>
+				<div className="flex flex-shrink-0 ">
+					<h1 className="font-bold text-xl cursor-pointer">
+						<span className="text-[#65968d]">Dekhanee:</span>
+						<span className="text-[#172f3d]">The Beautiful</span>
+					</h1>
+				</div>
+				<div className="dropdown float-right">
+					<button className="bg-transparent text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+						<svg className="w-8 h-8" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M4 6h16M4 12h16M4 18h16" />
+						</svg>
+					</button>
+
+
+					<ul className="link-container dropdown-menu hidden mr-2 md:mr-5 lg:mr-10">
+						<Link href="/"><a className="rounded-t navbar-link border border-gray-700">Home</a></Link>
+						<Link href="/about"><a className="navbar-link border border-gray-700 border-t-0">About</a></Link>
+						<Link href="/contact"><a className="navbar-link border border-gray-700 border-t-0">contact</a></Link>
+						<Link href="/products"><a className="rounded-b navbar-link  border border-gray-700 border-t-0">Products</a></Link>
+					</ul>
+				</div>
+				<div onClick={toggleCart} className="cart absolute right-0 top-7 mx-5 cursor-pointer ">
+					<AiOutlineShoppingCart className='text-xl md:text-2xl' />
+				</div>
+				<div ref={ref} className=" w-72 sideCart absolute top-0 right-0 bg-green-100 px-8 py-10 transform transition-transform translate-x-full">
+					<h2 className="font-bold underline text-xl text-center ">Shopping Cart</h2>
+					<span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl">
+						<AiFillCloseCircle />
+					</span>
+					<ol className="list-decimal font-semibold">
+						<li>
+							<div className="item flex my-5">
+								<div className="w-2/3 font-semibold">Ear-rings</div>
+								<div className="flex font-semibold items-center justify-center w-1/3 text-lg ">
+									<AiOutlineMinusCircle className="cursor-pointer" /><span className="mx-3">1</span><AiOutlinePlusCircle className="cursor-pointer" />
+								</div>
+							</div>
+						</li>
+						<li>
+							<div className="item flex my-5">
+								<div className="w-2/3 font-semibold">Ear-rings</div>
+								<div className="flex font-semibold items-center justify-center w-1/3 text-lg ">
+									<AiOutlineMinusCircle className="cursor-pointer" /><span className="mx-3">1</span><AiOutlinePlusCircle className="cursor-pointer" />
+								</div>
+							</div>
+						</li>
+						<li>
+							<div className="item flex my-5">
+								<div className="w-2/3 font-semibold">Ear-rings</div>
+								<div className="flex font-semibold items-center justify-center w-1/3 text-lg ">
+									<AiOutlineMinusCircle className="cursor-pointer" /><span className="mx-3">1</span><AiOutlinePlusCircle className="cursor-pointer" />
+								</div>
+							</div>
+						</li>
+						<li>
+							<div className="item flex my-5">
+								<div className="w-2/3 font-semibold">Ear-rings</div>
+								<div className="flex font-semibold items-center justify-center w-1/3 text-lg ">
+									<AiOutlineMinusCircle className="cursor-pointer" /><span className="mx-3">1</span><AiOutlinePlusCircle className="cursor-pointer" />
+								</div>
+							</div>
+						</li>
+						<li>
+							<div className="item flex my-5">
+								<div className="w-2/3 font-semibold">Ear-rings</div>
+								<div className="flex font-semibold items-center justify-center w-1/3 text-lg ">
+									<AiOutlineMinusCircle className="cursor-pointer" /><span className="mx-3">1</span><AiOutlinePlusCircle className="cursor-pointer" />
+								</div>
+							</div>
+						</li>
+
+
+
+
+					</ol>
+					<div className="flex">
+						<button className="flex mx-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">Checkout</button>
+						<button className="flex mx-2  text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">Clear Cart</button>
+					</div>
+				</div>
+
+			</nav>
+		</>
 	);
 }
 
