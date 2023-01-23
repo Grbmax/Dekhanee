0775d22547debe5243abd727dbf11a57367ef831
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2023-01-23 14:44:49 +0530

    admin login

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,37 +1,54 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useForm, Resolver } from 'react-hook-form';
 
-type Credentials = {
-    admin_id : string,
-    admin_pwd : string
-}
+type FormValues = {
+  admin_id: string;
+  admin_pwd: string;
+};
 
-function admin() {
-    const [term, setTerm] = useState('');
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.admin_id ? values : {},
+    errors: !values.admin_id
+      ? {
+          admin_id: {
+            type: 'required',
+            message: 'This is required.',
+          },
+        }
+      : {},
+  };
+};
 
-    const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(term)
-    }
+export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({ resolver });
+  const onSubmit = handleSubmit((data) => console.log(data));
+  
   return (
     <>
-    <div className='bg-[#313131] bg-opacity-40'>
-        <div className='flex flex-row justify-center items-center h-screen'>
+    <div className='bg-[#313131] bg-opacity-60'>
+        <div className='flex flex-row h-screen justify-center items-center'>
             <div className='w-full max-w-xs'>
-                <form onSubmit={submitForm} className='bg-[#F4E9CD] shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-                    <div className='mb-4'>
+                <form onSubmit={onSubmit} className='bg-[#F4E9CD] shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+                <div className='mb-4'>
                         <label className='block text-gray-700 text-sm font-bold mb-2'>
                             Username
                         </label>
-                        <input value={term} onChange={(e) => setTerm(e.target.value)} 
-                        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='username' type='text' placeholder='Username'/>
+                        <input {...register("admin_id")}  
+                        className='shadow appearance-none border 
+                        rounded w-full py-2 px-3 text-gray-700 leading-tight 
+                        focus:outline-none focus:shadow-outline' 
+                        id='username' type='text' placeholder='Username'/>
+                        {errors?.admin_id && <p className='py-1 text-red-500 leading-tight text-xs italic'>{errors.admin_id.message}</p>}
                     </div>
                     <div className='mb-6'>
                         <label className='block text-gray-700 text-sm font-bold mb-2'>
                             Password
                         </label>
-                        <input value={term} onChange={(e) => setTerm(e.target.value)} 
-                        className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' type='password' placeholder='Password'/>
+                        <input {...register("admin_pwd")}  
+                        className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 
+                        text-gray-700 mb-3 leading-tight focus:outline-none 
+                        focus:shadow-outline' type='password' placeholder='Password'/>
                         <p className='text-red-500 text-xs italic'>Enter your password.</p>
                     </div>
                     <div className='flex items-center justify-center'>
@@ -41,14 +58,13 @@ function admin() {
                     </div>
                     <br></br>
                     <p className='text-center text-gray-500 text-xs'>
-                 &copy;2023 GRY. Corp. All rights reserved. 
-                </p>
+                 &copy;Dekkhanee : The Beautiful. All rights reserved. 
+                </p> 
                 </form>
             </div>
         </div>
-    </div>    
+    </div>
     </>
-  )
+  );
 }
 
-export default admin
