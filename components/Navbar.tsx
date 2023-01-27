ee1e834cec996ef340cbfa18910d4cb761ce994e
Yash Shirke <51786748+yash5201@users.noreply.github.com>
2023-01-27 17:19:00 +0530

    working on product page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hamburger from "./Hamburger";
 import Link from "next/link";
-import { AiOutlineShoppingCart, AiOutlineCloseCircle, AiFillMinusCircle, AiFillPlusCircle,AiOutlineUser} from 'react-icons/ai'
+import { AiOutlineShoppingCart, AiOutlineCloseCircle, AiFillMinusCircle, AiFillPlusCircle, AiOutlineUser } from 'react-icons/ai'
 import { useRef } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
@@ -11,8 +11,8 @@ const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
 
   //Session 
 
-  const {data:session} = useSession()
-  
+  const { data: session } = useSession()
+
   //DEUBG
   // console.log("Session Details: ", session);
 
@@ -37,31 +37,31 @@ const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
 
 
       <div className="flex items-center justify-center shadow-md bg-[#F4E9CD] w-full text-black z-10">
-        
+
         <div className=" w-1/3">
           <Hamburger />
         </div>
-        
+
         <div id="logo" className=" flex justify-center text-center w-1/3 text-black text-2xl md:text-4xl lg:text-5xl italic font-semibold">
-        <Image src={DekhaneeLogo} alt="profilepciture" height={8*14} width={8*14} className='p-2'/>
+          <Image src={DekhaneeLogo} alt="profilepciture" height={8 * 14} width={8 * 14} className='p-2' />
           {/* <Link href={""}>Dekhanee</Link> */}
         </div>
-        
+
         <div className="w-1/3 p-5">
-          
+
           <div className="flex space-x-2 justify-end ">
             {session?.user ? (
               <>
-              <p> {session.user.name} </p>
-              <button onClick={() => signOut()} className="border-2 border-black p-1 rounded-lg hover:bg-black hover:text-white">
-                Sign Out
-              </button></>
+                <p> {session.user.name} </p>
+                <button onClick={() => signOut()} className="border-2 border-black p-1 rounded-lg hover:bg-black hover:text-white">
+                  Sign Out
+                </button></>
             )
-             : (
-              <button onClick={() => signIn()} className="border-2 border-black p-2 rounded-lg hover:bg-black hover:text-white">
-              Register / Login
-              </button>
-             )}
+              : (
+                <button onClick={() => signIn()} className="border-2 border-black p-2 rounded-lg hover:bg-black hover:text-white">
+                  Register / Login
+                </button>
+              )}
 
             <button onClick={toggleCart}>
               <AiOutlineShoppingCart className="h-6 w-6 items-center" />
@@ -69,9 +69,10 @@ const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
           </div>
 
 
-          <div ref={ref} className={`sideCart w-72 h-[100vh] absolute top-0 right-0 bg-[#F4E9CD] p-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0': 'translate-x-full'}`}>
+          <div ref={ref} className={`sideCart w-72 h-[100vh] absolute top-0 right-0 bg-[#F4E9CD]  p-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-full': 'translate-x-0'}`}>
+          {/* <div ref={ref} className={`sideCart w-72 h-[100vh] absolute top-0 right-0 bg-[#F4E9CD] px-8 py-10 transform transition-transform translate-x-full`}>
+ */}            <h2 className="font-bold text-xl text-center"> Shoping Cart </h2>
             <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl"><AiOutlineCloseCircle /></span>
-            <h2 className="font-bold text-xl text-center"> Shoping Cart </h2>
             <ol className="list-decimal font-semibold">
               {Object.keys(cart).length == 0 &&
                 <div className="my-4 font-semibold">No items in the cart</div>}
@@ -92,7 +93,7 @@ const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
             <Link href={'/checkout'}><button className="flex mt-16 border-0 border-black py-2 px-8 focus:outline-none">Checkout</button></Link>
             <button onClick={clearCart} className="flex mt-16 border-2 border-black py-2 px-8 focus:outline-none">clear cart</button>
           </div>
-          
+
         </div>
       </div>
     </>
--- pages/earrings.tsx
+++ pages/earrings.tsx
@@ -1,115 +1,53 @@
 import React from 'react'
 import Link from 'next/link'
+import { InferGetServerSidePropsType } from 'next'
 
-const earrings = () => {
+
+type Cards = {
+  id: number,
+  p_name: string,
+  p_type: string,
+  p_category: string,
+  p_inventory_id: string
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`http://localhost:3000/api/products/fetchList`)
+  const cards: Cards[] = await res.json()
+  return {
+    props: {
+      cards,
+    },
+  }
+}
+
+function earrings({ cards }: InferGetServerSidePropsType<typeof getStaticProps>) {
+  //for(var i = 0; i < Object.keys(cards).length; i++)
+  // {
+  //     console.log(cards[i])
+  // 
   return (
-
-    <div>
+    <>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center p-5">
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl ">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48 rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/1.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/2.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                  <p className="mt-1">$21.15</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/3.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                  <p className="mt-1">$12.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/4.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                  <p className="mt-1">$18.40</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/5.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                  <p className="mt-1">$16.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/6.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                  <p className="mt-1">$21.15</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/7.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                  <p className="mt-1">$12.00</p>
-                </div>
-              </Link>
-            </div>
-            <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-2xl">
-              <Link href={'/product/earing'}>
-                <div className="block relative h-48  rounded overflow-hidden ">
-                  <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="../ear-rings/8.jpeg" />
-                </div>
-                <div className="mt-4 text-center md:text-left">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                  <p className="mt-1">$18.40</p>
-                </div>
-              </Link>
+            <div className='flex flex-row justify-center h-screen items-center w-full'>
+              {
+                cards.map((card) =>
+                <>
+
+                <p> {card.id} </p> <br/>
+                <p> {card.p_name} </p> <br/>
+                <p> {card.p_type} </p> <br/>
+
+                </>
+          )
+              }
             </div>
           </div>
         </div>
       </section>
-    </div>
-
+    </>
   )
 }
-
 export default earrings
